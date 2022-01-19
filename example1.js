6ea46081c269069a81a05139322795bbeaf87f9e
dcompt4 <71852647+dcompt4@users.noreply.github.com>
2022-01-19 13:54:24 -0600

    Added first problem

---
+++ example1.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(800, 400);
+  }
+  
+function draw() {
+    background(115, 243, 59);
+    ellipse(200,200,300,300);
+    rect(200,600,300,300);
+    rect(400,50,300,300);
+}
