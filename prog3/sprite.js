24864c5d5ef5a24399503ebef5f5ac63b5e7eeff
dcompt4 <71852647+dcompt4@users.noreply.github.com>
2022-02-03 08:59:32 -0600

    updated some new sprites

--- prog3/sprite.js
+++ prog3/sprite.js
@@ -20,6 +20,9 @@ let locationChange = 0;
 function preload() {
   spelunky = loadImage('SpelunkyGuy.png');
   green = loadImage('Green.png');
+  roundBoy = loadImage('RoundBoy.png');
+  roundGirl = loadImage('RoundGirl.png');
+  yellow = loadImage('Yellow.png');
 }
 
 function setup() {
@@ -33,29 +36,38 @@ function draw() {
 
   push();
   //Underground background
-  fill(101, 78, 56);
+  fill(128, 98, 69);
   noStroke();
-  rect(0, window.innerHeight/4, window.innerWidth, window.innerHeight);
+  rect(0, window.innerHeight/4, window.innerWidth, window.innerHeight/4);
   //Underground background
-  fill(101, 78, 56);
+  fill(76, 59, 43);
   noStroke();
-  rect(0, window.innerHeight/4, window.innerWidth, window.innerHeight);
+  rect(0, window.innerHeight/4*2, window.innerWidth, window.innerHeight/4*2);
   //Dirt ground
   fill(155, 118, 83);
   noStroke();
   rect(0, window.innerHeight/4, window.innerWidth, 100);
   //Dirt ground level -1
-  fill(128, 98, 69);
+  fill(101, 78, 56);
   noStroke();
   rect(0, window.innerHeight/4*2, window.innerWidth, 100);
   //Dirt ground level -2
-  fill(128, 98, 69);
+  fill(52, 41, 31);
   noStroke();
-  rect(0, window.innerHeight/4*3, window.innerWidth, 100);
+  rect(0, window.innerHeight/4*3, window.innerWidth, window.innerHeight/4);
   //Grass
   fill(52, 140, 49);
   noStroke();
   rect(0, window.innerHeight/4, window.innerWidth, 10);
+  stroke(52, 140, 49);
+  strokeWeight(4);
+  for(var i = 0; i < window.innerWidth; i += 5) {
+    line(i, window.innerHeight/4, i, window.innerHeight/4-3);
+  }
+  //Sun
+  fill(244, 233, 155);
+  noStroke();
+  ellipse(0,0,150,150);
   //Sky bridge
   // fill(170);
   // noStroke();
@@ -63,13 +75,13 @@ function draw() {
   pop();
 
   push();
-  newSprite(500, window.innerHeight/4 - 70, green);
+  newSprite(500, window.innerHeight/4 - 70, yellow);
   pop();
   push();
   newSprite(900, window.innerHeight/4 - 70, spelunky);
   pop();
   push();
-  newSprite(0, window.innerHeight/4 - 70, spelunky);
+  newSprite(0, window.innerHeight/4 - 70, roundBoy);
   pop();
 
   push();
@@ -77,7 +89,7 @@ function draw() {
   pop();
 
   push();
-  newSprite(60, window.innerHeight/4*3 - 70, spelunky);
+  newSprite(60, window.innerHeight/4*3 - 70, roundGirl);
   pop();
   
 
@@ -98,8 +110,8 @@ function newSprite(locationX, locationY, character) {
   if ((keyIsPressed == true) && (keyCode === LEFT_ARROW)) {
     
     newScale = -1.0;
-    locationChange += -1;
-    time += .1;
+    locationChange += -0.5;
+    time += .05;
     if (time > 1) {
       index++;
       if (index > spriteMap.length - 1) {
@@ -111,8 +123,8 @@ function newSprite(locationX, locationY, character) {
   } else if ((keyIsPressed == true) && (keyCode === RIGHT_ARROW)) {
     
     newScale = 1.0;
-    locationChange += 1;
-    time += .1;
+    locationChange += 0.5;
+    time += .05;
     if (time > 1) {
       index++;
       if (index > spriteMap.length - 1) {
