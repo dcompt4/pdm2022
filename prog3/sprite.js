8746cf51d2735ae271eecb671d9d1e1c9b1476a4
dcompt4 <71852647+dcompt4@users.noreply.github.com>
2022-02-06 15:47:48 -0600

    changed project to use classes instead of plain functions

--- prog3/sprite.js
+++ prog3/sprite.js
@@ -1,48 +1,46 @@
-let animation;
-let spriteMap = [
-  { x: 0, y: 0 },
-  { x: 80, y: 0 },
-  { x: 160, y: 0 },
-  { x: 240, y: 0 },
-  { x: 320, y: 0 },
-  { x: 400, y: 0 },
-  { x: 480, y: 0 },
-  { x: 560, y: 0 },
-  { x: 640, y: 0 },
-]
-let index = 0;
-let spriteSheet;
-let spriteWidth = 80;
-let spriteHeight = 80;
+let spelunkyCharacter, greenCharacter, roundBoyCharacter, roundGirlCharacter, yellowCharacter, robotCharacter;
+let cyclopsCharacter, blueCharacter, redCharacter, limeCharacter, meatCharacter, eskimoCharacter;
 
-let frame = 0;
 
-let newScale;
-
-let time = 0;
-let time2 = 0;
-
-let locationChange = 0;
-let changeLocation = 0;
 
 function preload() {
+  //Loads all sprite sheets
   spelunky = loadImage('SpelunkyGuy.png');
-  green = loadImage('Green.png');
+  greenChar = loadImage('Green.png');
   roundBoy = loadImage('RoundBoy.png');
   roundGirl = loadImage('RoundGirl.png');
-  yellow = loadImage('Yellow.png');
+  yellowChar = loadImage('Yellow.png');
   robot = loadImage('Robot.png');
   cyclops = loadImage('Cyclops.png');
-  blue = loadImage('Blue.png');
-  red = loadImage('Red.png');
-  lime = loadImage('Lime.png');
+  blueChar = loadImage('Blue.png');
+  redChar = loadImage('Red.png');
+  limeChar = loadImage('Lime.png');
   meat = loadImage('Meat.png');
   eskimo = loadImage('Eskimo.png');
 }
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-    background(255, 255, 255);
+    // Fixes scale(-1) issue
+    imageMode(CENTER);
+
+    // Top Layer
+    greenCharacter = new Character(greenChar, 100, window.innerHeight/4 - 30);
+    roundBoyCharacter = new Character(roundBoy, 400, window.innerHeight/4 - 30);
+    roundGirlCharacter = new Character(roundGirl, 700, window.innerHeight/4 - 30);
+    limeCharacter = new Character(limeChar, 1000, window.innerHeight/4 - 30);
+
+    // Mid Layer
+    redCharacter = new Character(redChar, 300, window.innerHeight/4*2 - 30);
+    yellowCharacter = new Character(yellowChar, 600, window.innerHeight/4*2 - 30);
+    blueCharacter = new Character(blueChar, 900, window.innerHeight/4*2 - 30);
+    eskimoCharacter = new Character(eskimo, 1200, window.innerHeight/4*2 - 30);
+
+    // Bottom Layer
+    spelunkyCharacter = new Character(spelunky, 200, window.innerHeight/4*3 - 30);
+    robotCharacter = new Character(robot, 500, window.innerHeight/4*3 - 30);
+    cyclopsCharacter = new Character(cyclops, 800, window.innerHeight/4*3 - 30);
+    meatCharacter = new Character(meat, 1100, window.innerHeight/4*3 - 30);
     
 }
 
@@ -51,12 +49,10 @@ function draw() {
 
   push();
   //Underground background
-  //fill(128, 98, 69);
   fill(125);
   noStroke();
   rect(0, window.innerHeight/4, window.innerWidth, window.innerHeight/4);
   //Underground background
-  //fill(76, 59, 43);
   fill(25);
   noStroke();
   rect(0, window.innerHeight/4*2, window.innerWidth, window.innerHeight/4*2);
@@ -104,82 +100,88 @@ function draw() {
   pop();
 
   
-  cloud(80, 70);
-  cloud(250, 70);
-  cloud(575, 70);
-  cloud(925, 70);
-  cloud(1250, 70);
-  cloud(1425, 70);
-  cloud(1560, 70);
-  cloud(1825, 70);
+  // cloud(80, 70);
+  // cloud(250, 70);
+  // cloud(575, 70);
+  // cloud(925, 70);
+  // cloud(1250, 70);
+  // cloud(1425, 70);
+  // cloud(1560, 70);
+  // cloud(1825, 70);
 
-  cloud(0, 35);
-  cloud(150, 35);
-  cloud(500, 35);
-  cloud(800, 35);
-  cloud(1050, 35);
-  cloud(1200, 35);
-  cloud(1490, 35);
-  cloud(1725, 35);
-  cloud(1900, 35);
+  // cloud(0, 35);
+  // cloud(150, 35);
+  // cloud(500, 35);
+  // cloud(800, 35);
+  // cloud(1050, 35);
+  // cloud(1200, 35);
+  // cloud(1490, 35);
+  // cloud(1725, 35);
+  // cloud(1900, 35);
   
 
-
-
-  // First implementation sprites (Running)
-
-  push();
-  newSprite(600, window.innerHeight/4 - 70, green);
-  pop(); 
-
-  push();
-  newSprite(1360, window.innerHeight/4 - 70, eskimo);
-  pop(); 
-
-  push();
-  newSprite(950, window.innerHeight/4*2 - 70, yellow);
-  pop();
-
-  push();
-  newSprite(300, window.innerHeight/4*3 - 70, cyclops);
-  pop();
-
-  push();
-  newSprite(1600, window.innerHeight/4*3 - 70, spelunky);
-  pop();
   
-  // Second implementation sprites (Walking)
-  push();
-  sprite(150, window.innerHeight/4 - 70, roundBoy);
-  pop();
-
-  push();
-  sprite(1100, window.innerHeight/4 - 70, roundGirl);
-  pop();
-
-  push();
-  sprite(1650, window.innerHeight/4 - 70, lime);
-  pop();
-
-  push();
-  sprite(500, window.innerHeight/4*2 - 70, blue);
-  pop();
-
-  push();
-  sprite(1550, window.innerHeight/4*2 - 70, red);
-  pop();
-
-  push();
-  sprite(650, window.innerHeight/4*3 - 70, robot);
-  pop();
-
-  push();
-  sprite(1200, window.innerHeight/4*3 - 70, meat);
-  pop();
-
-  
-
+  greenCharacter.draw();
+  roundBoyCharacter.draw();
+  roundGirlCharacter.draw();
+  limeCharacter.draw();
+  redCharacter.draw();
+  yellowCharacter.draw();
+  blueCharacter.draw();
+  eskimoCharacter.draw();
+  spelunkyCharacter.draw();
+  robotCharacter.draw();
+  cyclopsCharacter.draw();
+  meatCharacter.draw();
 }
+
+function keyPressed() {
+  if(keyCode == RIGHT_ARROW) {
+    greenCharacter.go(1);
+    roundBoyCharacter.go(1);
+    roundGirlCharacter.go(1);
+    limeCharacter.go(1);
+    redCharacter.go(1);
+    yellowCharacter.go(1);
+    blueCharacter.go(1);
+    eskimoCharacter.go(1);
+    spelunkyCharacter.go(1);
+    robotCharacter.go(1);
+    cyclopsCharacter.go(1);
+    meatCharacter.go(1);
+
+  } else if (keyCode == LEFT_ARROW) {
+    greenCharacter.go(-1);
+    roundBoyCharacter.go(-1);
+    roundGirlCharacter.go(-1);
+    limeCharacter.go(-1);
+    redCharacter.go(-1);
+    yellowCharacter.go(-1);
+    blueCharacter.go(-1);
+    eskimoCharacter.go(-1);
+    spelunkyCharacter.go(-1);
+    robotCharacter.go(-1);
+    cyclopsCharacter.go(-1);
+    meatCharacter.go(-1);
+  }
+}
+
+function keyReleased() {
+  greenCharacter.stop();
+  roundBoyCharacter.stop();
+  roundGirlCharacter.stop();
+  limeCharacter.stop();
+  redCharacter.stop();
+  yellowCharacter.stop();
+  blueCharacter.stop();
+  eskimoCharacter.stop();
+  spelunkyCharacter.stop();
+  robotCharacter.stop();
+  cyclopsCharacter.stop();
+  meatCharacter.stop();
+}
+
+
 
 function cloud(cloudX, cloudY) {
   fill(255);
@@ -191,88 +193,41 @@ function cloud(cloudX, cloudY) {
   ellipse(cloudX+45, cloudY-10, 40, 20);
 }
 
-// First implementation of animating sprites
-// Uses map and indices to change frame of sprite animation
-function newSprite(locationX, locationY, character) {
-
-  cell = spriteMap[index];
-  let sprite = character.get(cell.x, cell.y, spriteWidth, spriteHeight);
-  
-  if(newScale == -1.0) {
-    translate((locationX + locationChange)*2 + 80,0);
-  }
-  scale(newScale, 1.0);
-  image(sprite, locationX + locationChange, locationY);
-
-  if ((keyIsPressed == true) && (keyCode === LEFT_ARROW)) {
-    
-    newScale = -1.0;
-    locationChange += -0.3;
-    time += .04;
-    if (time > 1) {
-      index++;
-      if (index > spriteMap.length - 1) {
-        index = 0;
-      }
-      time = 0;
-    }
-
-  } else if ((keyIsPressed == true) && (keyCode === RIGHT_ARROW)) {
-    
-    newScale = 1.0;
-    locationChange += 0.3;
-    time += .04;
-    if (time > 1) {
-      index++;
-      if (index > spriteMap.length - 1) {
-        index = 0;
-      }
-      time = 0;
-    }
-
-  } else {
-    
-    index = 0;
-
+class Character {
+  constructor(character, x, y) {
+    this.character = character;
+    this.x = x;
+    this.y = y;
+    this.move = 0;
+    this.facing = 1;
   }
 
+  draw() {
+    push();
+    translate(this.x,this.y);
+    scale(this.facing, 1);
 
+    if(this.move == 0) {
+      image(this.character, 0, 0, 80, 80, 0, 0, 80, 80);
+    } else {
+      image(this.character, 0, 0, 80, 80, 80 * (this.sx + 1), 0, 80, 80);
+    }
 
-}
+    if(frameCount % 5 == 0) {
+      this.sx = (this.sx + 1) % 8;
+    }
 
-// Second implementation of animating sprites
-// Uses image() function to change frame using coordinates in large sprite sheet
-function sprite(x, y, character) {
-  if(newScale == -1.0) {
-    translate((x + changeLocation)*2 + 80,0);
+    this.x += 2 * this.move;
+    pop();
   }
-  scale(newScale, 1.0);
-  image(character, x + changeLocation, y, 80, 80, 80 * frame, 0, 80, 80);
 
+  go(direction) {
+    this.move = direction;
+    this.facing = direction;
+    this.sx = 3;
+  }
 
-  if ((keyIsPressed == true) && (keyCode === LEFT_ARROW)) {
-    
-    newScale = -1.0;
-    changeLocation += -0.1;
-    time2 += .015;
-    if (time2 > 1) {
-      frame = (frame + 1) % 9;
-      time2 = 0;
-    }
-
-  } else if ((keyIsPressed == true) && (keyCode === RIGHT_ARROW)) {
-    
-    newScale = 1.0;
-    changeLocation += 0.1;
-    time2 += .015;
-    if (time2 > 1) {
-      frame = (frame + 1) % 9;
-      time2 = 0;
-    }
-
-  } else {
-    
-    frame = 0;
-
+  stop() {
+    this.move = 0;
   }
 }
