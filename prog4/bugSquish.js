8d4deb65fd5d1c6376b6e4760d1445cbb64d550f
dcompt4 <71852647+dcompt4@users.noreply.github.com>
2022-02-14 13:18:34 -0600

    almost finished bug squish

--- prog4/bugSquish.js
+++ prog4/bugSquish.js
@@ -3,7 +3,12 @@ let cyclopsCharacter, blueCharacter, redCharacter, limeCharacter, meatCharacter,
 let array = [];
 let numEnemies = 40;
 let gameState = 'wait';
-
+let score = 0;
+let highScore = 0;
+let startTime;
+let timeLeft;
+let startDirection = [-1, 1];
+let bugWave = 1;
 
 function preload() {
   //Loads all sprite sheets
@@ -15,7 +20,6 @@ function setup() {
     // Fixes scale(-1) issue
     imageMode(CENTER);
 
-    let startDirection = [-1, 1];
     for(let i = 0; i < numEnemies; i++) {
       array[i] = new Character(bug, random(50, window.innerWidth-50), random(50, window.innerHeight - 50), random(2, 5), random(startDirection));
     }
@@ -33,7 +37,41 @@ function draw() {
     for(let i = 0; i < numEnemies; i++) {
       array[i].draw();
     }
+
+    let myTime = 30 - timer();
+    textAlign(LEFT, TOP);
+    text('Timer: ' + myTime, 20, 20, width);
+    text('Score: ' + score, 20, 50, width);
+
+    if(score > 0 && score % (bugWave * numEnemies) == 0) {
+      for(let n = 0; n < numEnemies; n++) {
+        array[n] = new Character(bug, random(50, window.innerWidth-50), random(50, window.innerHeight - 50), random(2, 5), random(startDirection));
+      }
+      bugWave++;
+    }
+
+    if(myTime <= 0) {
+      gameState = 'end';
+
+      if(score > highScore) {
+        highScore = score;
+      }
+    }
+
+
   } else if(gameState == 'end') {
+
+    textSize(30);
+    textAlign(CENTER, CENTER);
+    text('Game Over', 0, height/2 - 90, width);
+    text('Score: ' + score, 0, height/2 - 30, width);
+    text('High Score: ' + highScore, 0, height/2, width);
+  
+    text('Press space to start new game!', 0, height/2 + 60, width);
+
+    
+    
+
 
   }
 
@@ -47,16 +85,41 @@ function draw() {
 
 function keyPressed() {
   if(keyCode == 32 && gameState == 'wait') {
+    startTime = millis();
     gameState = 'start';
-  } else if (keyCode == 32) {
-    
+  } else if (keyCode == 32 && gameState == 'end') {
+
+    for(let p = 0; p < numEnemies; p++) {
+      array[p] = new Character(bug, random(50, window.innerWidth-50), random(50, window.innerHeight - 50), random(2, 5), random(startDirection));
+    }
+
+    score = 0
+    startTime = millis();
+    gameState = 'start';
   }
 }
 
 function mousePressed() {
-  for(let i = 0; i < numEnemies; i++) {
-    array[i].squish();
+  if(gameState == 'start') {
+
+    for(let i = 0; i < numEnemies; i++) {
+
+      if(!(array[i].squished)) {
+        array[i].squish();
+        if(array[i].squished) {
+          score++;
+          for(let j = 0; j < numEnemies; j++) {
+            array[j].speed += 0.05;
+          }
+        }
+      }
+    }
+
   }
+}
+
+function timer() {
+  return int((millis() - startTime) / 1000);
 }
 
 
