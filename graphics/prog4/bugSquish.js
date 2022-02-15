3aef43956ef5e6f1e17e524caafae34fef72f18a
dcompt4 <71852647+dcompt4@users.noreply.github.com>
2022-02-15 13:54:24 -0600

    fixed layout and fixed end game round reset on bug squish

--- graphics/prog4/bugSquish.js
+++ graphics/prog4/bugSquish.js
@@ -93,7 +93,7 @@ function keyPressed() {
       array[p] = new Character(bug, random(50, window.innerWidth-50), random(50, window.innerHeight - 50), random(2, 5), random(startDirection));
     }
 
-    round = 1;
+    bugWave = 1;
     score = 0;
     startTime = millis();
     gameState = 'start';
