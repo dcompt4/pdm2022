08e8e2f98397a6ea3c4c2d6418b01ac8c342b96f
dcompt4 <71852647+dcompt4@users.noreply.github.com>
2022-04-07 17:05:28 -0500

    Flappy Graphics mostly done just need collision with pipes and refinement

--- flappy.js
+++ flappy.js
@@ -1,5 +1,8 @@
 let array = [];
 let player;
+let numObstacles;
+let obstacles = [];
+let score = 0;
 let gameState = 'paused';
 
 
@@ -12,11 +15,20 @@ function setup() {
     imageMode(CENTER);
 
     player = new Player(window.innerWidth/2, window.innerHeight/2);
-    
+
+    numObstacles = round(window.innerWidth/300) + 1;
+
+    for(let i = 1; i <= numObstacles; i++) {
+      obstacles[i-1] = new Obstacle(window.innerWidth/2 + 300*i, random(window.innerHeight/2 - 100, window.innerHeight/2 + 100), 3);
+    }
 }
 
 function draw() {
   background(113, 188, 225);
+
+  for(let i = 1; i <= numObstacles; i++) {
+    obstacles[i-1].draw();
+  }
 
   push();
   fill(52, 140, 49);
@@ -32,11 +44,56 @@ function draw() {
 
   player.draw();
 
+  if(gameState == 'end') {
+    push();
+    fill(0);
+    textSize(100);
+    textAlign(CENTER, TOP);
+    text('SCORE', 0, height/2 - 100, width);
+    text(score, 0, height/2, width);
+    pop();
+
+    push();
+    fill(0);
+    textAlign(CENTER, TOP);
+    textSize(30);
+    text('Press to Restart', 0, height/2 + 100, width);
+    pop();
+  } else if (gameState == 'paused'){
+    push();
+    fill(0);
+    textSize(50);
+    textAlign(CENTER, TOP);
+    text('SCORE', 0, 20, width);
+    text(score, 0, 70, width);
+    pop();
+
+    push();
+    fill(0);
+    textAlign(CENTER, TOP);
+    textSize(20);
+    text('Press Space/Button to Go', 0, player.y - 50, width);
+    pop();
+  } else {
+    push();
+    fill(0);
+    textSize(50);
+    textAlign(CENTER, TOP);
+    text('SCORE', 0, 20, width);
+    text(score, 0, 70, width);
+    pop();
+  }
+
+  
 }
 
 function keyPressed() {
+
   if(keyCode == 32 && gameState == 'paused') {
     player.go();
+    for(let i = 1; i <= numObstacles; i++) {
+      obstacles[i-1].go();
+    }
     gameState = 'playing';
   }
 
@@ -46,8 +103,22 @@ function keyPressed() {
 
   if(keyCode == 32 && gameState == 'end') {
     player.reset();
+    for(let i = 1; i <= numObstacles; i++) {
+      obstacles[i-1].reset();
+    }
+
+    score = 0;
     gameState = 'paused';
   }
+
+  if(keyCode == 27 && gameState == 'playing') {
+    player.pause();
+    for(let i = 1; i <= numObstacles; i++) {
+      obstacles[i-1].pause();
+    }
+    gameState = 'paused';
+  }
+  
 
 }
 
@@ -55,27 +126,59 @@ function keyReleased() {
   
 }
 
-class Cloud {
+class Obstacle {
   constructor(x, y, speed) {
+    this.originalX = x;
     this.x = x;
     this.y = y;
-    this.speed = speed;
+    this.tempSpeed = speed;
+    this.speed = 0;
+    this.scoreCounted = false;
   }
 
   draw() {
-    fill(255);
-    noStroke();
-    ellipse(this.x, this.y, 40, 20);
-    ellipse(this.x+30, this.y, 40, 20);
-    ellipse(this.x+60, this.y, 40, 20);
-    ellipse(this.x+15, this.y-10, 40, 20);
-    ellipse(this.x+45, this.y-10, 40, 20);
+    push();
+    fill(52, 140, 49);
+    stroke(0);
+    // Top Pipe
+    rect(this.x - 10, 0, 40, this.y - 100);
+    rect(this.x - 12, this.y - 110, 44, 20);
+    // Bottom Pipe
+    rect(this.x - 10, this.y + 100, 40, window.innerHeight - this.y + 30);
+    rect(this.x - 12, this.y + 90, 44, 20);
 
-    this.x += this.speed;
 
-    if(this.x > window.innerWidth + 100) {
-      this.x = -100;
+    this.x -= this.speed;
+
+    if(this.x < window.innerWidth/2 && !this.scoreCounted) {
+      score++;
+      this.scoreCounted = true;
     }
+
+    if(this.x < -44) {
+      this.x = 300*numObstacles - 44;
+      this.y = random(window.innerHeight/2 - 100, window.innerHeight/2 + 100);
+      this.scoreCounted = false;
+    }
+
+    if(gameState == 'end') {
+      this.speed = 0;
+    }
+    pop();
+  }
+
+  go() {
+    this.speed = this.tempSpeed;
+  }
+
+  pause() {
+    this.speed = 0;
+  }
+
+  reset() {
+    this.speed = 0;
+    this.x = this.originalX;
+    this.scoreCounted = false;
   }
 
 
@@ -112,11 +215,11 @@ class Player {
   }
 
   jump() {
-    this.velocity = -250;
+    this.velocity = -400;
   }
 
   go() {
-    this.acceleration = 10;
+    this.acceleration = 20;
   }
 
   pause() {
